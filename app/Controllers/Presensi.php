fe8f76a73053d0a4b884ddda950b47b23eeed6aa
danuri <danualbantani@gmail.com>
2024-05-15 11:54:17 +0700

    Update Presensi.php

--- app/Controllers/Presensi.php
+++ app/Controllers/Presensi.php
@@ -11,4 +11,31 @@ class Presensi extends BaseController
     {
         return view('presensi');
     }
+
+    public function checkin()
+    {
+      if (! $this->validate([
+          'nip' => "required",
+          'password' => "required"
+        ])) {
+            return $this->response->setJSON(['error'=>true,'message'=>'Harap isi dengan lengkap.']);
+        }
+
+        $client = \Config\Services::curlrequest();
+
+        $ip = $this->request->getVar('ip');
+        $lat = $this->request->getVar('lat');
+        $lon = $this->request->getVar('lon');
+
+        $param = ['LAT'=>$lat,'LON'=>$lon];
+
+        // try {
+          $response = $client->request('GET', $server.'/mobile/home/absen/'.$ip.'?'.http_build_query($param), [
+              'verify' => false
+          ]);
+
+          $response = json_decode($response->getBody());
+
+          return $this->response->setJSON(['error'=>false,'message'=>$response]);
+    }
 }
