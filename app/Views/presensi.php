7a5e260545b1168ac5d0bc0870d18e239c36e25f
danuri <danualbantani@gmail.com>
2024-08-27 06:39:09 +0700

    ssss

--- app/Views/presensi.php
+++ app/Views/presensi.php
@@ -8,17 +8,12 @@
         <div class="card-body d-flex align-items-center">
           <div class="user-profile me-3">
             <img src="img/bg-img/2.jpg" alt="">
-            <i class="bi bi-pencil"></i>
-            <form action="#">
-              <input class="form-control" type="file">
-            </form>
           </div>
           <div class="user-info">
             <div class="d-flex align-items-center">
-              <h5 class="mb-1">Affan Islam</h5>
-              <span class="badge bg-warning ms-2 rounded-pill">Pro</span>
+              <h5 class="mb-1"><?= session('nama')?></h5>
+              <span class="badge bg-warning ms-2 rounded-pill"><?= session('nip')?></span>
             </div>
-            <p class="mb-0">UX/UI Designer</p>
           </div>
         </div>
       </div>
--- app/Views/template.php
+++ app/Views/template.php
@@ -6,14 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Affan - PWA Mobile HTML Template">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
   <meta name="theme-color" content="#0134d4">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
   <!-- Title -->
-  <title>Affan - PWA Mobile HTML Template</title>
+  <title>Simpeg Personal - Biro Kepegawaian Kementerian Agama RI</title>
 
   <!-- Favicon -->
   <link rel="icon" href="<?= base_url()?>assets/img/core-img/favicon.ico">
